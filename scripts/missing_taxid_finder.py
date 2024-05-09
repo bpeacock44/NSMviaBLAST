84ec5e4796945cb0fb944968c956a33cc8be2f29
Beth Peacock <32349239+bpeacock44@users.noreply.github.com>
2024-05-09 11:57:47 -0500

    Create missing_taxid_finder.py

---
+++ scripts/missing_taxid_finder.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+# Check if the correct number of command-line arguments is provided
+if len(sys.argv) != 2:
+    print("Usage: python your_script.py directory_path")
+    sys.exit(1)
+
+directory_path = sys.argv[1]  # Get the directory path from the command-line argument
+
+# Define the filenames based on the directory path
+input_file = os.path.join(directory_path, "all_combined.diamondout.mod")
+output_file = os.path.join(directory_path, "missing_taxids.txt")
+temp_file = os.path.join(directory_path, "all_combined.diamondout.mod2")
+
+with open(input_file, 'r') as infile, open(output_file, 'w') as outfile, open(temp_file, 'w') as temp:
+    for line in infile:
+        if line.startswith("#"):
+            temp.write(line)
+            continue
+
+        columns = line.strip().split('\t')
+        if len(columns) == 10 and all(columns):
+            temp.write(line)
+        else:
+            outfile.write(line)
